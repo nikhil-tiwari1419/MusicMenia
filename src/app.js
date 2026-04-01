96d4d7cad462320f888b2b3ffd31c5728fb197ad
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2026-04-01 20:28:59 +0530

    commitiing new lines of code

--- src/app.js
+++ src/app.js
@@ -7,15 +7,24 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-origin: process.env.CLIENT_URL || 'http://localhost:5173',
-credentials :true,
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            process.env.CLIENT_URL, 'http://localhost:5173'
+        ].filter(Boolean); // Filter out any undefined values
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CROS'));
+        }
+    },
+    credentials: true,
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
 
-app.get("/", (req,res)=>res.send("Api is working"))
+app.get("/", (req, res) => res.send("Api is working"))
 app.use('/api/auth', authRouter);
 app.use('/api/music', musicRoutes);
 
