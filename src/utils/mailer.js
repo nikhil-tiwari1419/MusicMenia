34a8c30f8800706f357ce22acc6b7d9819138152
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2026-04-14 13:47:00 +0530

    secure: false

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
